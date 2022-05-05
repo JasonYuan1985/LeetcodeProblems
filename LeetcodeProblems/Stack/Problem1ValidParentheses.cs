02d78bcdca531ba988d3346b5d3eb176963be1f9
yuan.jy <Jason_Yuan@epam.com>
2022-05-05 14:20:39 +0800

    Complete Stack1 using other person solution

---
+++ LeetcodeProblems/Stack/Problem1ValidParentheses.cs
@@ -0,0 +1,75 @@
+﻿namespace LeetcodeProblems.Stack
+{
+    public class Problem1ValidParentheses
+    {
+        //public bool IsValid(string s)
+        //{
+        //    Stack<char> bracketsStack = new Stack<char>();
+
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+        //        //check char, if (,{,[, then enqueue
+        //        if (s[i] == '(')
+        //        {
+        //            bracketsStack.Push(')');
+        //        }
+        //        else if (s[i] == '{')
+        //        {
+        //            bracketsStack.Push('}');
+        //        }
+        //        else if (s[i] == '[')
+        //        {
+        //            bracketsStack.Push(']');
+        //        }
+        //        else if (bracketsStack.Count > 0 && bracketsStack.Peek() == s[i])
+        //        {
+        //            //other if found ),},] then dequeue 
+        //            bracketsStack.Pop();
+        //        }
+        //        else if (s[i] == ')' || s[i] == '}' || s[i] == ']')
+        //        {
+        //            return false;
+        //        }
+        //    }
+
+        //    return bracketsStack.Count == 0;
+        //}
+
+        public bool IsValid(string s)
+        {
+            Stack<char> blah = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    blah.Push(c);
+                    continue;
+                }
+
+                switch (c)
+                {
+                    case ')':
+                        if (blah.Count == 0 || blah.Pop() != '(')
+                        {
+                            return false;
+                        }
+                        break;
+                    case '}':
+                        if (blah.Count == 0 || blah.Pop() != '{')
+                        {
+                            return false;
+                        }
+                        break;
+                    case ']':
+                        if (blah.Count == 0 || blah.Pop() != '[')
+                        {
+                            return false;
+                        }
+                        break;
+                }
+            }
+            return blah.Count == 0;
+        }
+    }
+}
---
+++ LeetcodeProblemsTest/Stack/Problem1ValidParenthesesTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetcodeProblems.Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Stack.Tests
+{
+    [TestClass()]
+    public class Problem1ValidParenthesesTests
+    {
+        [TestMethod()]
+        public void IsValid_Example4_With_1_Full_RoundBrackets_Return_True()
+        {
+            Problem1ValidParentheses solution = new Problem1ValidParentheses();
+
+            string inputString = "()";
+            Assert.AreEqual(true, solution.IsValid(inputString));
+        }
+
+        [TestMethod()]
+        public void IsValid_Example5_With_Mix_Full_Brackets_Return_False()
+        {
+            Problem1ValidParentheses solution = new Problem1ValidParentheses();
+
+            string inputString = "([)]";
+            Assert.AreEqual(false, solution.IsValid(inputString));
+        }
+
+        [TestMethod()]
+        public void IsValid_Example6_With_One_Close_Brackets_Return_False()
+        {
+            Problem1ValidParentheses solution = new Problem1ValidParentheses();
+
+            string inputString = "]";
+            Assert.AreEqual(false, solution.IsValid(inputString));
+        }
+    }
+}
