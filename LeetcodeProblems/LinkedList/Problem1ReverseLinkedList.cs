214573f9e16f1bd82a65b4425d260009b2ce1ec7
Jason Yuan <cn093476@gmail.com>
2022-05-11 23:52:33 +0800

    Reserve Linked List

---
+++ LeetcodeProblems/LinkedList/Problem1ReverseLinkedList.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetcodeProblems.LinkedList
+{
+    public class Problem1ReverseLinkedList
+    {
+        /**
+         * Definition for singly-linked list.
+         * public class ListNode {
+         *     public int val;
+         *     public ListNode next;
+         *     public ListNode(int val=0, ListNode next=null) {
+         *         this.val = val;
+         *         this.next = next;
+         *     }
+         * }
+         */
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode reverseListHead = null;
+            if (head != null)
+            {
+                ListNode oldNode = head;
+                reverseListHead = new ListNode(oldNode.val);
+
+                while (oldNode != null && oldNode.next != null)
+                {
+                    ListNode nextNode = reverseListHead;
+                    reverseListHead = new ListNode(oldNode.next.val, nextNode);
+                    oldNode = oldNode.next;
+                }
+            }
+
+            return reverseListHead;
+        }
+    }
+}
---
+++ LeetcodeProblemsTest/LinkedList/Problem1ReverseLinkedListTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static LeetcodeProblems.LinkedList.Problem1ReverseLinkedList;
+
+namespace LeetcodeProblems.LinkedList.Tests
+{
+    [TestClass()]
+    public class Problem1ReverseLinkedListTests
+    {
+        [TestMethod()]
+        public void ReverseListTest()
+        {
+            Problem1ReverseLinkedList solution = new Problem1ReverseLinkedList();
+            var head = new ListNode(1, new ListNode(2, null));
+            var result = solution.ReverseList(head);
+            Assert.AreEqual(2, result.val);
+            Assert.AreEqual(1, result.next.val);
+        }
+    }
+}
