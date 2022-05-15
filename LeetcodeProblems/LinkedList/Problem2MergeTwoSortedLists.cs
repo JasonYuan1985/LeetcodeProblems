5621bc4801f73f64f8f57a061806d025e06fa987
yuan.jy <Jason_Yuan@epam.com>
2022-05-15 17:51:48 +0800

    Merge Two sorted lists

---
+++ LeetcodeProblems/LinkedList/Problem2MergeTwoSortedLists.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.LinkedList
+{
+    public class Problem2MergeTwoSortedLists
+    {
+        /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode comparelist1 = list1;
+            ListNode comparelist2 = list2;
+            ListNode finalResult = new ListNode();
+            ListNode temp = finalResult;
+
+            //get the start of the node
+            //finalResult = this.IsFirstNodeAhead(comparelist1, comparelist2) ? comparelist1 : comparelist2;
+            //temp = finalResult;
+
+            while (comparelist1 != null && comparelist2 != null)
+            {
+                //compare each node, change the link
+                if (this.IsFirstNodeAhead(comparelist1, comparelist2))
+                {
+                    temp.next = comparelist1;
+                    comparelist1 = comparelist1.next;
+                }
+                else
+                {
+                    temp.next = comparelist2;
+                    comparelist2 = comparelist2.next;
+                }
+
+                temp = temp.next;
+            }
+
+            temp.next = GetUnFinishListNodeToFinalNext(comparelist1,comparelist2);
+
+            return finalResult.next;
+        }
+
+        private bool IsFirstNodeAhead(ListNode firstNode, ListNode secondNode)
+        {
+            return firstNode.val < secondNode.val;
+        }
+
+        private ListNode GetUnFinishListNodeToFinalNext(ListNode firstNode, ListNode secondNode)
+        {
+            return firstNode == null ? secondNode : firstNode;
+        }
+    }
+}
---
+++ LeetcodeProblemsTest/LinkedList/Problem2MergeTwoSortedListsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetcodeProblems.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetcodeProblems.LinkedList.Problem2MergeTwoSortedLists;
+
+namespace LeetcodeProblems.LinkedList.Tests
+{
+    [TestClass()]
+    public class Problem2MergeTwoSortedListsTests
+    {
+        [TestMethod()]
+        public void MergeTwoListsTest()
+        {
+            Problem2MergeTwoSortedLists solution = new Problem2MergeTwoSortedLists();
+            var list1 = new ListNode(1,new ListNode(2,new ListNode(4,null)));
+            var list2 = new ListNode(1, new ListNode(3, new ListNode(4, null)));
+            var resultList = solution.MergeTwoLists(list1, list2);
+            Assert.AreEqual(1, resultList.val);
+            Assert.AreEqual(1, resultList.next.val);
+            Assert.AreEqual(2, resultList.next.next.val);
+            Assert.AreEqual(3, resultList.next.next.next.val);
+            Assert.AreEqual(4, resultList.next.next.next.next.val);
+            Assert.AreEqual(4, resultList.next.next.next.next.next.val);
+        }
+    }
+}
