d3535740edf97db635e5bebf966649b7d0c2f722
yuan.jy <Jason_Yuan@epam.com>
2022-05-15 12:43:54 +0800

    use some effiective method from leetcode

--- LeetcodeProblems/LinkedList/Problem1ReverseLinkedList.cs
+++ LeetcodeProblems/LinkedList/Problem1ReverseLinkedList.cs
@@ -27,21 +27,44 @@
 
         public ListNode ReverseList(ListNode head)
         {
-            ListNode reverseListHead = null;
-            if (head != null)
-            {
-                ListNode oldNode = head;
-                reverseListHead = new ListNode(oldNode.val);
+            //ListNode reverseListHead = null;
+            //if (head != null)
+            //{
+            //    ListNode oldNode = head;
+            //    reverseListHead = new ListNode(oldNode.val);
 
-                while (oldNode != null && oldNode.next != null)
-                {
-                    ListNode nextNode = reverseListHead;
-                    reverseListHead = new ListNode(oldNode.next.val, nextNode);
-                    oldNode = oldNode.next;
-                }
+            //    while (oldNode != null && oldNode.next != null)
+            //    {
+            //        ListNode nextNode = reverseListHead;
+            //        reverseListHead = new ListNode(oldNode.next.val, nextNode);
+            //        oldNode = oldNode.next;
+            //    }
+            //}
+
+            //return reverseListHead;
+
+            //ListNode curr = head, prev = null, next = null;
+
+            //while (curr != null)
+            //{
+            //    next = curr.next;
+            //    curr.next = prev;
+            //    prev = curr;
+            //    curr = next;
+            //}
+
+            ListNode current, previous = null,  next = null;
+            current = head;
+
+            while (current != null)
+            {
+                next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
             }
 
-            return reverseListHead;
+            return previous;
         }
     }
 }
--- LeetcodeProblemsTest/LinkedList/Problem1ReverseLinkedListTests.cs
+++ LeetcodeProblemsTest/LinkedList/Problem1ReverseLinkedListTests.cs
@@ -15,5 +15,18 @@ namespace LeetcodeProblems.LinkedList.Tests
             Assert.AreEqual(2, result.val);
             Assert.AreEqual(1, result.next.val);
         }
+
+        [TestMethod()]
+        public void ReverseList_1_2_3_4_5()
+        {
+            Problem1ReverseLinkedList solution = new Problem1ReverseLinkedList();
+            var head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
+            var result = solution.ReverseList(head);
+            Assert.AreEqual(5, result.val);
+            Assert.AreEqual(4, result.next.val);
+            Assert.AreEqual(3, result.next.next.val);
+            Assert.AreEqual(2, result.next.next.next.val);
+            Assert.AreEqual(1, result.next.next.next.next.val);
+        }
     }
 }
