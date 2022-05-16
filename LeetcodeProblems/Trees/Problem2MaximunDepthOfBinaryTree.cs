4d1a86a9fcb3b4277548ed618fe348995ff407da
yuan.jy <Jason_Yuan@epam.com>
2022-05-16 09:05:07 +0800

    maximum-depth-of-binary-tree

---
+++ LeetcodeProblems/Trees/Problem2MaximunDepthOfBinaryTree.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetcodeProblems.Trees
+{
+    public class Problem2MaximunDepthOfBinaryTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public int MaxDepth(TreeNode root)
+        {
+            TreeNode returnNode = root;
+            //recursion
+            if (returnNode == null)
+            {
+                return 0;
+            }
+            else
+            {
+                var leftMaxDepth = this.MaxDepth(returnNode.left);
+                var rightMaxDepth = this.MaxDepth(returnNode.right);
+                return (leftMaxDepth > rightMaxDepth ? leftMaxDepth : rightMaxDepth) + 1;
+            }
+        }
+    }
+}
---
+++ LeetcodeProblemsTest/Trees/Problem2MaximunDepthOfBinaryTreeTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetcodeProblems.Trees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetcodeProblems.Trees.Problem2MaximunDepthOfBinaryTree;
+
+namespace LeetcodeProblems.Trees.Tests
+{
+    [TestClass()]
+    public class Problem2MaximunDepthOfBinaryTreeTests
+    {
+        [TestMethod()]
+        public void MaxDepth_Example1_3_9_20_null_null_15_7_Return_3()
+        {
+            //[3,9,20,null,null,15,7]
+            Problem2MaximunDepthOfBinaryTree solution = new Problem2MaximunDepthOfBinaryTree();
+            var root = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20,
+                    new TreeNode(15), new TreeNode(7)));
+
+            Assert.AreEqual(3, solution.MaxDepth(root));
+        }
+
+        [TestMethod()]
+        public void MaxDepth_Example2_1_null_2_Return_2()
+        {
+            //[1,null,2]
+            Problem2MaximunDepthOfBinaryTree solution = new Problem2MaximunDepthOfBinaryTree();
+            var root = new TreeNode(1,
+                null,
+                new TreeNode(2));
+
+            Assert.AreEqual(2, solution.MaxDepth(root));
+        }
+    }
+}
