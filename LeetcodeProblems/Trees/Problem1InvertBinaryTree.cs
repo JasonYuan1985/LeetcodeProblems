fde7b6eb3ac26f67875d1d93873f1e281b839432
yuan.jy <Jason_Yuan@epam.com>
2022-05-16 08:56:04 +0800

    invert binary tree

---
+++ LeetcodeProblems/Trees/Problem1InvertBinaryTree.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetcodeProblems.Trees
+{
+    public class Problem1InvertBinaryTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public TreeNode InvertTree(TreeNode root)
+        {
+            TreeNode returnNode = root;
+            //recursion
+            if(returnNode == null)
+            {
+                return null;
+            }
+            else
+            {
+                TreeNode temp = returnNode.left;
+                returnNode.left = returnNode.right;
+                returnNode.right = temp;
+                this.InvertTree(returnNode.left);
+                this.InvertTree(returnNode.right);
+                return returnNode;
+            }
+        }
+    }
+}
---
+++ LeetcodeProblemsTest/Trees/Problem1InvertBinaryTreeTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetcodeProblems.Trees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetcodeProblems.Trees.Problem1InvertBinaryTree;
+
+namespace LeetcodeProblems.Trees.Tests
+{
+    [TestClass()]
+    public class Problem1InvertBinaryTreeTests
+    {
+        [TestMethod()]
+        public void InvertTree_Example1_3Layers()
+        {
+            TreeNode tree = new TreeNode(4,
+                new TreeNode(2,
+                    new TreeNode(1), new TreeNode(3)),
+                new TreeNode(7,
+                    new TreeNode(6), new TreeNode(9))
+                );
+            Problem1InvertBinaryTree solution = new Problem1InvertBinaryTree();
+            var result = solution.InvertTree(tree);
+            Assert.AreEqual(7, result.left.val);
+            Assert.AreEqual(9, result.left.left.val);
+            Assert.AreEqual(6, result.left.right.val);
+            Assert.AreEqual(2, result.right.val);
+            Assert.AreEqual(3, result.right.left.val);
+            Assert.AreEqual(1, result.right.right.val);
+        }
+    }
+}
